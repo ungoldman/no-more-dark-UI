12e580441fad52b2d36a8c4ec2d3353026e688a4
Morgane Santos <morganesantos@gmail.com>
2016-11-13 13:30:50 -0800

    lmao

---
+++ twitter-blanket.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  coverMeLikeABlanket = function() {
+    $(".dashboard-left").hide();
+    $(".dashboard-right").hide();
+    $(".moments").hide();
+  };
+
+  coverMeLikeABlanket();
+
+});
